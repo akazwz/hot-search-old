32aee674291b01c6fed07bfff8852977d38c2abf
zwz <akazwz@pm.me>
2021-08-28 22:39:37 +0800

    update

--- src/pages/HotSearchData.js
+++ src/pages/HotSearchData.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {message, Input, Divider, Col, Row,} from 'antd';
 import {ClearOutlined} from '@ant-design/icons';
 import HotSearchRank from '../components/HotSearchRank';
