0cc137b681ff60f7042ff1357259a490c0538394
akez <1791189210@qq.com>
2021-08-30 11:42:46 +0800

    encode url component

--- src/components/HotSearch.js
+++ src/components/HotSearch.js
@@ -11,12 +11,13 @@ const HotSearch = (props) => {
                 dataSource={props.hotSearch}
                 renderItem={(item) => {
                     const {rank, content, hot, topic_lead} = item
+                    const contentUrl = encodeURIComponent(content);
                     return (
                         (
                             <List.Item>
                                 <List.Item.Meta
                                     avatar={<Avatar>{rank}</Avatar>}
-                                    title={<Link to={"/hot-search-data/" + content}><span
+                                    title={<Link to={"/hot-search-data/" + contentUrl}><span
                                         className="content-span">{content}</span>&nbsp;<span
                                         className="hot-span">{hot}</span></Link>}
                                     description={topic_lead}
