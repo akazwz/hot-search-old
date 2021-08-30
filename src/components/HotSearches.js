93f7a707e19c7ea089ce4421d6f10a4d913ac6ca
akez <1791189210@qq.com>
2021-08-30 14:39:39 +0800

    热搜历史界面展示

---
+++ src/components/HotSearches.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HotSearches = () => {
+    return (
+        <div>
+            this is hot searches.
+        </div>
+    );
+};
+
+export default HotSearches;
--- src/pages/HistoryHotSearches.js
+++ src/pages/HistoryHotSearches.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import {Empty} from 'antd';
+import React, {useState} from 'react';
+import {Empty, DatePicker, Space, TimePicker, Row, Col, Button} from 'antd';
+import {ArrowRightOutlined} from '@ant-design/icons';
+import HotSearches from '../components/HotSearches';
+import {GetHotSearchesByDuration} from "../api/hot-search";
 
 const HistoryHotSearches = () => {
-    return (
-        <div>
-            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>
-        </div>
-    );
-};
+        const [startDateStr, setStartDateStr] = useState('');
+        const [startTimeStr, setStartTimeStr] = useState('-00-00');
+
+        const [endDateStr, setEndDateStr] = useState('');
+        const [endTimeStr, setEndTimeStr] = useState('-00-00');
+
+
+        const [showHotSearches, setShowHotSearches] = useState(false);
+
+        const handleStartDatePickerOnChange = (date, dateString) => {
+            setStartDateStr(dateString);
+        }
+        const handleStartTimePickerOnChange = (time, timeString) => {
+            setStartTimeStr("-" + timeString.slice(0, 2) + "-" + timeString.slice(3, 5));
+        }
+
+        const handleEndDatePickerOnChange = (date, dateString) => {
+            setEndDateStr(dateString);
+        }
+        const handleEndTimePickerOnChange = (time, timeString) => {
+            setEndTimeStr("-" + timeString.slice(0, 2) + "-" + timeString.slice(3, 5));
+        }
+
+        const handleBtnStartToSearchOnClick = () => {
+            const start = startDateStr + startTimeStr;
+            const end = endDateStr + endTimeStr;
+            GetHotSearchesByDuration(start, end)
+                .then((res) => {
+                    console.log(res.data);
+                    if (res.status === 200) {
+                        setShowHotSearches(true);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        };
+
+        return (
+            <div>
+                <Row className="date-time-picker">
+                    <Col span={8}>
+                        <Space direction="vertical">
+                            {"开始时间"}
+                            <DatePicker onChange={handleStartDatePickerOnChange}/>
+                            <TimePicker minuteStep={15} format="HH:mm" onChange={handleStartTimePickerOnChange}/>
+                        </Space>
+                    </Col>
+                    <Col span={8}>
+                        <ArrowRightOutlined/>
+                    </Col>
+                    <Col span={8}>
+                        <Space direction="vertical">
+                            {"结束时间"}
+                            <DatePicker onChange={handleEndDatePickerOnChange}/>
+                            <TimePicker minuteStep={15} format="HH:mm" onChange={handleEndTimePickerOnChange}/>
+                        </Space>
+                    </Col>
+                </Row>
+                <Row className="date-time-picker">
+                    <Col span={24}>
+                        <Button
+                            disabled={startDateStr.length !== 10 || endDateStr.length !== 10}
+                            onClick={handleBtnStartToSearchOnClick}
+                        >
+                            开始搜索
+                        </Button>
+                    </Col>
+                </Row>
+                {showHotSearches ? <HotSearches/> : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>}
+            </div>
+        );
+    }
+;
 
 export default HistoryHotSearches;
