9e243b59425d3201c03132c0ad7ff1c05d4adb74
akez <1791189210@qq.com>
2021-08-31 10:22:20 +0800

    热搜历史的切换

--- src/components/HotSearches.js
+++ src/components/HotSearches.js
@@ -1,29 +1,80 @@
 import React, {useEffect, useState} from 'react';
-import {Tag, Row, Col, Affix} from 'antd';
-import {LeftCircleOutlined, RightCircleOutlined, ClockCircleOutlined} from '@ant-design/icons';
+import {Tag, Row, Col, Affix, Button} from 'antd';
+import {
+    ClockCircleOutlined,
+    LeftOutlined,
+    RightOutlined
+} from '@ant-design/icons';
 import HotSearch from "./HotSearch";
 
 const HotSearches = (props) => {
     const propSearches = props.searches;
     const [hotSearch, setHotSearch] = useState([]);
     const [top, setTop] = useState(100);
-
+    const [index, setIndex] = useState(0);
+    const [dateTime, setDateTime] = useState("1998-03-07 21:59:07");
     useEffect(() => {
         const height = window.innerHeight;
         setTop(height / 2);
         if (propSearches.length > 0) {
-            const {searches} = propSearches[0];
+            const {searches, time} = propSearches[index];
             setHotSearch(searches);
+            setDateTime(time);
         }
+    }, [index, propSearches])
 
-    }, [propSearches])
+    const handleBtnNextOnClick = () => {
+        if (propSearches.length > 0) {
+            if (index === propSearches.length - 1) {
+                setIndex(0);
+                const {searches, time} = propSearches[0];
+                setHotSearch(searches);
+                setDateTime(time);
+                return;
+            }
+            const {searches, time} = propSearches[index + 1];
+            setHotSearch(searches);
+            setDateTime(time);
+            setIndex(index + 1);
+        }
+    };
+
+    const handleBtnLastOnClick = () => {
+        if (propSearches.length > 0) {
+            if (index === 0) {
+                setIndex(propSearches.length - 1);
+                const {searches, time} = propSearches[propSearches.length - 1];
+                setHotSearch(searches);
+                setDateTime(time);
+                return;
+            }
+            const {searches, time} = propSearches[index - 1];
+            setHotSearch(searches);
+            setDateTime(time);
+            setIndex(index - 1);
+        }
+    };
 
     return (
         <div>
             <Row className="show-single-hot-search">
+                <Col span={24}>
+                    <Affix offsetTop={17}>
+                        <Tag icon={<ClockCircleOutlined/>}>
+                            {dateTime}
+                        </Tag>
+                    </Affix>
+                </Col>
+            </Row>
+
+            <Row className="show-single-hot-search">
                 <Col span={2}>
                     <Affix offsetTop={top}>
-                        <LeftCircleOutlined className="arrow-change-hot-search"/>
+                        <Button
+                            shape="circle"
+                            size="large"
+                            icon={<LeftOutlined className="arrow-change-hot-search"
+                                                onClick={handleBtnLastOnClick}/>}/>
                     </Affix>
                 </Col>
                 <Col span={20}>
@@ -31,7 +82,11 @@ const HotSearches = (props) => {
                 </Col>
                 <Col span={2}>
                     <Affix offsetTop={top}>
-                        <RightCircleOutlined className="arrow-change-hot-search"/>
+                        <Button
+                            shape="circle"
+                            size="large"
+                            icon={<RightOutlined className="arrow-change-hot-search"
+                                                 onClick={handleBtnNextOnClick}/>}/>
                     </Affix>
                 </Col>
             </Row>
