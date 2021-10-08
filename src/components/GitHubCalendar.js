6ee785fcef5f1be30084844436c6cff7db766ca0
akazwz <akazwz@pm.me>
2021-10-08 11:51:15 +0800

    完成设备自适应

--- src/components/GitHubCalendar.js
+++ src/components/GitHubCalendar.js
@@ -1,53 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as echarts from "echarts";
 
 const GitHubCalendar = (props) => {
     const calendar = useRef(null);
-    const initOrUpdateEcharts = () => {
-        let myChart = echarts.getInstanceByDom(calendar.current);
-        if (!myChart) {
-            myChart = echarts.init(calendar.current, null, {renderer: 'svg'});
-        }
-        myChart.on('click', (param) => {
-        });
-        myChart.setOption({
-            title: {},
-            tooltip: {
-                formatter: function (params) {
-                    if (params.data[2] === 0) {
-                        return "no contributions on " + params.data[0].toString();
-                    }
-                    return params.data[2].toString() + " contributions on " + params.data[0].toString();
-                }
-            },
-            visualMap: {
-                show: false,
-                min: 0,
-                max: 4,
-                inRange: {
-                    color: ['#ffffff', '#39d353', '#26a641', '#006d32', '#0e4429'],
-                },
-            },
-            calendar: {
-                top: 30,
-                left: 30,
-                right: 30,
-                cellSize: ['auto', 13],
-                range: '2021',
-                itemStyle: {
-                    borderWidth: 0.5
-                },
-                yearLabel: {show: false}
-            },
-            series: {
-                type: 'heatmap',
-                coordinateSystem: 'calendar',
-                data: props.data,
-            }
-        });
-    };
-
+    const [height, setHeight] = useState(100);
     const handleResize = () => {
+        if (calendar.current != null) {
+            setHeight(calendar.current.clientWidth / 7);
+        }
         let myChart = echarts.getInstanceByDom(calendar.current);
         if (!myChart) {
             myChart = echarts.init(calendar.current);
@@ -55,20 +15,67 @@ const GitHubCalendar = (props) => {
         myChart.resize();
     };
 
+
     useEffect(() => {
-        initOrUpdateEcharts();
+        if (calendar.current != null) {
+            setHeight(calendar.current.clientWidth / 7);
+            setTimeout(()=> {
+                let myChart = echarts.getInstanceByDom(calendar.current);
+                if (!myChart) {
+                    myChart = echarts.init(calendar.current, null, {renderer: 'svg'});
+                }
+                myChart.on('click', (param) => {
+                });
+                myChart.setOption({
+                    title: {},
+                    tooltip: {
+                        formatter: function (params) {
+                            if (params.data[2] === 0) {
+                                return "no contributions on " + params.data[0].toString();
+                            }
+                            return params.data[2].toString() + " contributions on " + params.data[0].toString();
+                        }
+                    },
+                    visualMap: {
+                        show: false,
+                        min: 0,
+                        max: 4,
+                        inRange: {
+                            color: ['#ffffff', '#39d353', '#26a641', '#006d32', '#0e4429'],
+                        },
+                    },
+                    calendar: {
+                        cellSize: ['auto', 13],
+                        left: 'center',
+                        top: 'center',
+                        width: '100%',
+                        height: '100%',
+                        range: '2021',
+                        itemStyle: {
+                            borderWidth: 0.7
+                        },
+                        yearLabel: {show: false}
+                    },
+                    series: {
+                        type: 'heatmap',
+                        coordinateSystem: 'calendar',
+                        data: props.data,
+                    }
+                });
+            },0);
+        }
+
         window.addEventListener('resize', handleResize);
         return function cleanup() {
             window.removeEventListener('resize', handleResize);
         }
-    });
+    }, [props.data]);
 
     return (
         <div>
             <div style={{
-                width: '90%',
-                height: 350,
-                maxHeight: 600,
+                width: '100%',
+                height: height,
                 margin: 'auto',
             }} ref={calendar} />
         </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import {GithubOutlined} from '@ant-design/icons';
+import { GithubOutlined } from '@ant-design/icons';
 import GitHubCalendar from "../components/GitHubCalendar";
 import { GetGithubCalendarByUsername } from "../api/hot-search";
-import { message } from "antd";
+import { message, Divider, Spin, Input } from "antd";
 
 const About = () => {
     const [total, setTotal] = useState(0);
     const [data, setData] = useState([]);
+    const [showLoading, setShowLoading] = useState(true);
+    const [username, setUsername] = useState("akazwz");
     useEffect(() => {
-        GetGithubCalendarByUsername("akazwz").then((res) => {
+        GetGithubCalendarByUsername(username).then((res) => {
             // http status
             if (res.status !== 200) {
                 message.error("获取数据失败").then();
@@ -24,14 +26,15 @@ const About = () => {
             setTotal(total);
             let dataLater = [];
             for (let i = 0; i < contributions.length; i++) {
-                const {count,date,level} = contributions[i];
-                dataLater.push([date, level,count]);
+                const {count, date, level} = contributions[i];
+                dataLater.push([date, level, count]);
             }
             setData(dataLater);
+            setShowLoading(false);
         }).catch((err) => {
             message.error("获取数据失败" + err).then();
         });
-    }, []);
+    }, [username]);
 
     return (
         <div style={{
@@ -39,15 +42,18 @@ const About = () => {
             marginTop: 30,
         }}>
             <h2>赵文卓</h2>
-            <br/>
+            <Divider />
             <a href="https://github.com/akazwz">
                 <GithubOutlined style={{
                     fontSize: 30,
-                }}/>
+                }} />
             </a>
-            <br/>
-            <h3>{total} contributions in 2021</h3>
-            <GitHubCalendar data={data}/>
+            <Divider />
+            {showLoading ? <Spin /> : null}
+            <h3>{total} contributions in 2021 ({username})</h3>
+            <GitHubCalendar data={data} />
+            <Divider />
+            <Input placeholder="查看我的Github 贡献 Calendar"/>
         </div>
     );
 };
