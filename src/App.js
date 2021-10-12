03debccb1d71fc6db057ecd9cb1efd7f4f88fa00
akazwz <akazwz@pm.me>
2021-10-12 09:37:30 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Layout } from 'antd';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, BrowserRouter as Route } from 'react-router-dom'
 import Home from '../src/pages/Home';
 import MyHeader from './components/MyHeader';
 import MyFooter from './components/MyFooter';
@@ -18,20 +18,20 @@ function App() {
         <div className='App'>
             <Layout>
                 <Header id='header-custom'>
-                    <MyHeader/>
+                    <MyHeader />
                 </Header>
                 <Content id='content-custom'>
                     <Switch>
-                        <Route exact path='/' component={Home}/>
-                        <Route exact path='/history-hot-searches' component={HistoryHotSearches}/>
-                        <Route exact path='/hot-search-data/:content' component={HotSearchData}/>
-                        <Route exact path='/hot-search-data/' component={HotSearchData}/>
-                        <Route exact path='/about' component={About}/>
-                        <Route exact path='/sub' component={Sub}/>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/history-hot-searches' component={HistoryHotSearches} />
+                        <Route exact path='/hot-search-data/:content' component={HotSearchData} />
+                        <Route exact path='/hot-search-data/' component={HotSearchData} />
+                        <Route exact path='/about' component={About} />
+                        <Route exact path='/sub' component={Sub} />
                     </Switch>
                 </Content>
                 <Footer id='footer-custom'>
-                    <MyFooter/>
+                    <MyFooter />
                 </Footer>
             </Layout>
         </div>
